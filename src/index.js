a3e03a7adae0b7f3cfa1359a369f1c244c28f12f
rbhansali9 <rbhansali9@gatech.edu>
2020-10-25 20:51:12 -0700

    2-17-Converting Styling to JSX Format.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import ReactDOM from 'react-dom';
 
 //create react component
 const App = function () {
-    return <div> Hi there !</div>;
-}
+    return (
+        <div>
+            <label classs="label" htmlFor="name">Enter name: </label>
+            <input type="text" id="name"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}> Submit</button>
+        </div>
+    );
+};
 
 //Take the react component and show it to screen
 ReactDOM.render(
